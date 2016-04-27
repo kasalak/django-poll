1391f41313a80c022cadd73d92391cd5c303613b
Karthik <karthik.kasala@gmail.com>
2016-04-27 15:46:37 -0400

    fleshed out detail template further

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
@@ -10,11 +10,6 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise HTTP404("Question does not exist.")
-    return render(request, 'polls/index.html', {'question': question})
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
