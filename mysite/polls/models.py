59de2cb6db75c3440f664aa0471009395ffcdbc2
Karthik <karthik.kasala@gmail.com>
2016-04-27 11:57:01 -0400

    added admin superuser

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,25 +1,18 @@
+import datetime
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date= models.DateTimeField('date_published')
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-@python_2_unicode_compatible
-class Question(models.Model):
-    # ...
-    def __str__(self):
-        return self.question_text
-
-@python_2_unicode_compatible
-class Choice(models.Model):
-    # ...
     def __str__(self):
         return self.choice_text
