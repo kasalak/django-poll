b4ac13e26b207d4002391e96d5ac33922896e9c0
Karthik <karthik.kasala@gmail.com>
2016-05-04 16:04:38 -0400

    running first test for erroneous pub_date classifications

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+import datetime
+
+from django.utils import timezone
+from .models import Question
 
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """
+        was_published_recently() should return False for questions with a
+        pub_date in the future
+        """
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
