2b49158f793e21ff7cb5fac18e43032083c3ef87
Karthik <karthik.kasala@gmail.com>
2016-05-05 21:31:03 -0400

    added past question test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -23,8 +23,19 @@ class QuestionMethodTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEquals(response.context['latest_question_list'], [])
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
+    def test_index_view_with_a_past_question(self):
+        """
+        Questions with a pub_date in the past should be displayed on the index page
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+        
     def test_was_published_recently_with_future_questions(self):
         """
         was_published_recently() should return False for questions with a
