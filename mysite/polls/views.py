5a27bebe3aeae8700b1f00e721525cffa61b5529
Karthik <karthik.kasala@gmail.com>
2016-05-05 21:17:31 -0400

    improving views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 # Create your views here.
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
