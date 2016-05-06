513ab8afdc5edaaa27db867bfdcf425ef54562ce
Karthik <karthik.kasala@gmail.com>
2016-05-05 21:23:54 -0400

    added create question test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
 import datetime
-
+from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 from .models import Question
 
 # Create your tests here.
@@ -35,3 +35,12 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
+
+    def create_question(question_text, days):
+        """
+        Creates a question with the given 'question_text' and published the
+        given number of 'days' offest to now (negative for questions published
+        in the past, positive for questions published in the future)"""
+        time = timezone.now() + datetime.timedelta(days=days)
+        
+        return Question.objects.create(question_text, pub_date=time)
