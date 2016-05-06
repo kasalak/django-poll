6857e9656798f956b6269020b899080db065d16c
Karthik <karthik.kasala@gmail.com>
2016-05-05 21:19:06 -0400

    questions from back to the future will not be returned

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,9 +12,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Returns the last five published questions"""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        """Returns the last five published questions (not including those set to
+        be published in the future)."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
